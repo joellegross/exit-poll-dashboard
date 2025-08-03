f9abc4ec9dcb22f5e37411f7009d9816f1d59a5b
Joelle Gross <50854878+joellegross@users.noreply.github.com>
2025-08-03 17:12:38 -0400

    port specification

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ app.title = "Exit Poll Dashboard"
 
 app.layout = create_layout()
 
-# Register all app callbacks
 df_path = os.path.join("data", "datafile_paths_dynamic.csv")
 register_callbacks(app, df_path)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8050))
+    app.run(debug=True, host="0.0.0.0", port=port)
